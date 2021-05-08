0c9acd8a4ececec34551937c2d92d2b33513b325
minhogang <minhogang@yahoo.com>
2021-05-08 08:40:50 +0000

    huge changes

--- simulator.cc
+++ simulator.cc
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <iostream>
 #include <fstream>
 #include <vector>
@@ -10,234 +11,323 @@ using namespace std;
 const unsigned short X_MASK = (1 << 12) - 1;
 const unsigned short Y_MASK = (1 << 8) - 1;
 
-vector<unsigned short> lines;
-
-short memory[4095];
-
-short accumulator = 0;
-unsigned short stack_pointer = 4095;
-unsigned short program_counter = 0;
-unsigned short memory_address = 0;
-unsigned short memory_buffer = 0;
-unsigned short instruction_register = 0;
-
-struct instruction
-{
-    unsigned short opcode, value;
-};
+const unsigned short CODE_SEGMENT_OFFSET = 4095;
 
 const unsigned short NON_PC_INCR[] = {JPOS_OPCODE, JZER_OPCODE, JUMP_OPCODE, JNEG_OPCODE, JNZE_OPCODE, CALL_OPCODE, SWAP_OPCODE};
 
-/* Decouples a binary instruction into a struct with their opcode and operand */
-instruction parse_instruction(unsigned short n)
+struct instruction
 {
+    unsigned short opcode, operand;
+};
+
+class State
+{
+    vector<short> memory = vector<short>(CODE_SEGMENT_OFFSET);
+    short accumulator = 0;
+    unsigned short stack_pointer = CODE_SEGMENT_OFFSET;
+    unsigned short program_counter = CODE_SEGMENT_OFFSET;
+    unsigned short memory_address = 0;
+    short memory_buffer = 0;
+    unsigned short instruction_register = 0;
+
+public:
+    State(vector<short>);
+    unsigned short fetch_instruction();
+    instruction decode_instruction(unsigned short);
+    void execute_instruction(instruction);
+    bool _end();
+
+private:
+    void print_state()
+    {
+        printf("ac<-%d, sp<- %u, pc<- %u, mar<-%u, mbr<-%u, ir<-%s\n", accumulator, stack_pointer,
+               program_counter, memory_address, memory_buffer, bitset<16>(instruction_register).to_string().c_str());
+    }
+
+    short memory_set(unsigned short addr, short value)
+    {
+        MAR_set(addr);
+        MBR_set(value);
+        return memory[memory_address] = memory_buffer;
+    }
+
+    short memory_get(unsigned short addr)
+    {
+        MAR_set(addr);
+        return MBR_set(memory[memory_address]);
+    }
+
+    unsigned short MAR_set(unsigned short addr)
+    {
+        printf("Setting MAR to %u... ", addr);
+        memory_address = addr;
+        print_state();
+
+        return addr;
+    }
+    short MBR_set(short value)
+    {
+        printf("Setting MBR to %d... ", value);
+        memory_buffer = value;
+        print_state();
+
+        return value;
+    }
+    unsigned short PC_set(unsigned short value)
+    {
+        if (value == memory.size()) {
+            printf("End of program.");
+            program_counter = value;
+        } else {
+            printf("Setting PC to %u... ", value);
+            program_counter = value;
+        }
+        print_state();
+
+        return value;
+    }
+    short AC_set(short value)
+    {
+        printf("Setting AC to %d... ", value);
+        accumulator = value;
+        print_state();
+
+        return value;
+    }
+    short load(short x, short offset = 0)
+    {
+        return AC_set(memory_get(x + offset));
+    }
+
+    short store(short x, short offset = 0)
+    {
+        return memory_set(x + offset, accumulator);
+    }
+
+    short add(short x, short offset = 0)
+    {
+        return AC_set(accumulator + memory_get(x + offset));
+    }
+
+    short subtract(short x, short offset = 0)
+    {
+        return AC_set(accumulator - memory_get(x + offset));
+    }
+
+    short jump_positive(short x)
+    {
+        return accumulator >= 0 ? PC_set(CODE_SEGMENT_OFFSET + x - 1) : PC_set(program_counter + 1);
+    }
+
+    short jump_zero(short x)
+    {
+        return accumulator == 0 ? PC_set(CODE_SEGMENT_OFFSET + x - 1) : PC_set(program_counter + 1);
+    }
+
+    short jump_negative(short x)
+    {
+        return accumulator < 0 ? PC_set(CODE_SEGMENT_OFFSET + x - 1) : PC_set(program_counter + 1);
+    }
+
+    short jump_nonzero(short x)
+    {
+        return accumulator != 0 ? PC_set(CODE_SEGMENT_OFFSET + x - 1) : PC_set(program_counter + 1);
+    }
+
+    short jump(short x)
+    {
+        return PC_set(CODE_SEGMENT_OFFSET + x - 1);
+    }
+
+    short load_constant(short x)
+    {
+        return AC_set(x);
+    }
+
+    short call_procedure(short x)
+    {
+        stack_pointer -= 1;
+        memory_set(stack_pointer, program_counter);
+        return PC_set(CODE_SEGMENT_OFFSET + x - 1);
+    }
+
+    void push(bool indirect = false)
+    {
+        stack_pointer -= 1;
+
+        indirect ? memory[stack_pointer] =
+                       memory_set(stack_pointer, memory_get(accumulator))
+                 : memory_set(stack_pointer, accumulator);
+    }
+
+    void pop(bool indirect = false)
+    {
+
+        indirect ? memory[accumulator] =
+                       memory_set(accumulator, memory_get(stack_pointer))
+                 : AC_set(memory_get(stack_pointer));
+        stack_pointer += 1;
+    }
+
+    void _return()
+    {
+        PC_set(memory_get(stack_pointer));
+        stack_pointer += 1;
+    }
+
+    void swap_sp_pc()
+    {
+        short temp = stack_pointer;
+        stack_pointer = program_counter;
+        program_counter = temp;
+    }
+
+    short increment_sp(short y)
+    {
+        return stack_pointer += y;
+    }
+
+    short decrement_sp(short y)
+    {
+        return stack_pointer -= y;
+    }
+};
+
+State::State(vector<short> code)
+{
+    memory.insert(memory.end(), code.begin(), code.end());
+}
+
+unsigned short State::fetch_instruction()
+{
+    printf("Fetching instruction at address %u... ", program_counter);
+    print_state();
+    return instruction_register = memory_get(program_counter);
+}
+
+instruction State::decode_instruction(unsigned short n)
+{
+    printf("Decoding instruction in IR... ");
     instruction instr;
 
     if (n >= INSP_OPCODE)
     {
         instr.opcode = n & ~Y_MASK;
-        instr.value = n & Y_MASK;
+        instr.operand = n & Y_MASK;
     }
     else if (n >= PSHI_OPCODE)
     {
         instr.opcode = n;
-        instr.value = 0;
+        instr.operand = 0;
     }
     else
     {
         instr.opcode = n & ~X_MASK;
-        instr.value = n & X_MASK;
+        instr.operand = n & X_MASK;
     }
+    print_state();
     return instr;
 }
 
-short load(short x, short offset = 0)
+void State::execute_instruction(instruction instr)
 {
-    return accumulator = memory[x + offset];
+    printf("Executing instruction in IR %d... ", program_counter - 1);
+    print_state();
+
+    switch (instr.opcode)
+    {
+    case 0:
+        load(instr.operand);
+        break;
+    case STOD_OPCODE:
+        store(instr.operand);
+        break;
+    case ADDD_OPCODE:
+        add(instr.operand);
+        break;
+    case SUBD_OPCODE:
+        subtract(instr.operand);
+        break;
+    case JPOS_OPCODE:
+        jump_positive(instr.operand);
+        break;
+    case JZER_OPCODE:
+        jump_zero(instr.operand);
+        break;
+    case JUMP_OPCODE:
+        jump(instr.operand);
+        break;
+    case LOCO_OPCODE:
+        load_constant(instr.operand);
+        break;
+    case LODL_OPCODE:
+        load(instr.operand, stack_pointer);
+        break;
+    case STOL_OPCODE:
+        store(instr.operand, stack_pointer);
+        break;
+    case ADDL_OPCODE:
+        add(instr.operand, stack_pointer);
+        break;
+    case SUBL_OPCODE:
+        subtract(instr.operand, stack_pointer);
+        break;
+    case JNEG_OPCODE:
+        jump_negative(instr.operand);
+        break;
+    case JNZE_OPCODE:
+        jump_nonzero(instr.operand);
+        break;
+    case CALL_OPCODE:
+        call_procedure(instr.operand);
+        break;
+    case PSHI_OPCODE:
+        push(true);
+        break;
+    case POPI_OPCODE:
+        pop(true);
+        break;
+    case PUSH_OPCODE:
+        push(false);
+        break;
+    case POP_OPCODE:
+        pop(false);
+        break;
+    case RETN_OPCODE:
+        _return();
+        break;
+    case SWAP_OPCODE:
+        swap_sp_pc();
+        break;
+    case INSP_OPCODE:
+        increment_sp(instr.operand);
+        break;
+    case DESP_OPCODE:
+        decrement_sp(instr.operand);
+        break;
+    default:
+        cout << "Unknown instruction: " << instr.operand << endl;
+    }
+
+    if (find(begin(NON_PC_INCR), end(NON_PC_INCR), instr.opcode) == end(NON_PC_INCR))
+    {
+        PC_set(program_counter + 1);
+    }
+
+    printf("\n");
 }
 
-short store(short x, short offset = 0)
-{
-    return memory[x + offset] = accumulator;
-}
-
-short add(short x, short offset = 0)
-{
-    return accumulator += memory[x + offset];
-}
-
-short subtract(short x, short offset = 0)
-{
-    return accumulator -= memory[x + offset];
-}
-
-short jump_positive(short x)
-{
-    return accumulator >= 0 ? program_counter = x - 1 : program_counter += 1;
-}
-
-short jump_zero(short x)
-{
-    return accumulator == 0 ? program_counter = x - 1 : program_counter += 1;
-}
-
-short jump_negative(short x)
-{
-    return accumulator < 0 ? program_counter = x - 1 : program_counter += 1;
-}
-
-short jump_nonzero(short x)
-{
-    return accumulator != 0 ? program_counter = x - 1 : program_counter += 1;
-}
-
-short jump(short x)
-{
-    return program_counter = x - 1;
-}
-
-short load_constant(short x)
-{
-    return accumulator = x;
-}
-
-short call_procedure(short x)
-{
-    stack_pointer -= 1;
-    memory[stack_pointer] = program_counter;
-    return program_counter = x - 1;
-}
-
-void push(bool indirect = false)
-{
-    stack_pointer -= 1;
-    indirect ? memory[stack_pointer] = memory[accumulator] : memory[stack_pointer] = accumulator;
-}
-
-void pop(bool indirect = false)
-{
-    indirect ? memory[accumulator] = memory[stack_pointer] : accumulator = memory[stack_pointer];
-    stack_pointer += 1;
-}
-
-void _return()
-{
-    program_counter = memory[stack_pointer];
-    stack_pointer += 1;
-}
-
-void swap_sp_pc()
-{
-    short temp = stack_pointer;
-    stack_pointer = program_counter;
-    program_counter = temp;
-}
-
-short increment_sp(short x)
-{
-    return stack_pointer += x;
-}
-
-short decrement_sp(short x)
-{
-    return stack_pointer -= x;
-}
+bool State::_end() { return program_counter == memory.size(); }
 
 /* Simulates the program line by line, while printing out the contents of the registers. */
 
-int simulate()
+int simulate(State state)
 {
-    while (program_counter < lines.size())
+    while (!state._end())
     {
-        memory_address = program_counter;
-        memory_buffer = lines.at(program_counter);
-        instruction_register = memory_buffer;
-
-        instruction instr = parse_instruction(instruction_register);
-
-        switch (instr.opcode)
-        {
-        case 0:
-            load(instr.value);
-            break;
-        case STOD_OPCODE:
-            store(instr.value);
-            break;
-        case ADDD_OPCODE:
-            add(instr.value);
-            break;
-        case SUBD_OPCODE:
-            subtract(instr.value);
-            break;
-        case JPOS_OPCODE:
-            jump_positive(instr.value);
-            break;
-        case JZER_OPCODE:
-            jump_zero(instr.value);
-            break;
-        case JUMP_OPCODE:
-            jump(instr.value);
-            break;
-        case LOCO_OPCODE:
-            load_constant(instr.value);
-            break;
-        case LODL_OPCODE:
-            load(instr.value, stack_pointer);
-            break;
-        case STOL_OPCODE:
-            store(instr.value, stack_pointer);
-            break;
-        case ADDL_OPCODE:
-            add(instr.value, stack_pointer);
-            break;
-        case SUBL_OPCODE:
-            subtract(instr.value, stack_pointer);
-            break;
-        case JNEG_OPCODE:
-            jump_negative(instr.value);
-            break;
-        case JNZE_OPCODE:
-            jump_nonzero(instr.value);
-            break;
-        case CALL_OPCODE:
-            call_procedure(instr.value);
-            break;
-        case PSHI_OPCODE:
-            push(true);
-            break;
-        case POPI_OPCODE:
-            pop(true);
-            break;
-        case PUSH_OPCODE:
-            push(false);
-            break;
-        case POP_OPCODE:
-            pop(false);
-            break;
-        case RETN_OPCODE:
-            _return();
-            break;
-        case SWAP_OPCODE:
-            swap_sp_pc();
-            break;
-        case INSP_OPCODE:
-            increment_sp(instr.value);
-            break;
-        case DESP_OPCODE:
-            decrement_sp(instr.value);
-            break;
-        default:
-            cout << "Unknown instruction: " << instr.value << endl;
-        }
-
-        if (find(begin(NON_PC_INCR), end(NON_PC_INCR), instr.opcode) == end(NON_PC_INCR))
-        {
-            program_counter++;
-        }
-
-        cout << "ac<-" << accumulator << ", sp<-" << stack_pointer << ", pc<-" << program_counter 
-            << ", mar<-" << memory_address << ", mbr<-" << memory_buffer << ", ir<-" << 
-            bitset<16>(instruction_register).to_string() << endl;
+        unsigned short n = state.fetch_instruction();
+        instruction instr = state.decode_instruction(n);
+        state.execute_instruction(instr);
     }
 
     return 0;
@@ -248,10 +338,14 @@ int main(int argc, char **argv)
     ifstream src(argv[1]);
 
     string line;
+    vector<short> code;
+
     while (getline(src, line))
     {
-        lines.push_back((unsigned short)stoi(line, nullptr, 2));
+        code.push_back((short)stoi(line, nullptr, 2));
     }
 
-    simulate();
+    State state = State(code);
+
+    simulate(state);
 }
